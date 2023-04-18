973fb687a5f641e01ba446b9684e850a2eed3610
kamal-hutech <kamal@hutechsolutions.com>
2023-04-19 01:54:32 +0530

    Code Complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,56 @@
-import React from 'react'
-import Logo from '../assets/logo.svg'
+import React, { useEffect, useState } from "react"
+import Logo from "../assets/logo.svg"
 const Header = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "medium"
+  )
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme))
+    document.documentElement.removeAttribute("class")
+    document.documentElement.classList.add(theme)
+  }, [theme])
+
   return (
     <header>
-        <div className='logo'>
-            <img src={Logo} alt="TaskMate"></img>
-            <span>TaskMate</span>
-        </div>
-        <div className='themeSelector'>
-            <span className='light'></span>
-            <span className='medium'></span>
-            <span className='dark'></span>
-            <span className='gradientOne'></span>
-            <span className='gradientTwo'></span>
-            <span className='gradientThree'></span>
-        </div>
+      <div className="logo">
+        <img src={Logo} alt="TaskMate"></img>
+        <span>TaskMate</span>
+      </div>
+      <div className="themeSelector">
+        <span
+          onClick={() => setTheme("light")}
+          className={theme === "light" ? "light activeTheme" : "light"}
+        ></span>
+        <span
+          onClick={() => setTheme("medium")}
+          className={theme === "medium" ? "medium activeTheme" : "medium"}
+        ></span>
+        <span
+          onClick={() => setTheme("dark")}
+          className={theme === "dark" ? "dark activeTheme" : "dark"}
+        ></span>
+        <span
+          onClick={() => setTheme("gradientOne")}
+          className={
+            theme === "gradientOne" ? "gradientOne activeTheme" : "gradientOne"
+          }
+        ></span>
+        <span
+          onClick={() => setTheme("gradientTwo")}
+          className={
+            theme === "gradientTwo" ? "gradientTwo activeTheme" : "gradientTwo"
+          }
+        ></span>
+        <span
+          onClick={() => setTheme("gradientThree")}
+          className={
+            theme === "gradientThree"
+              ? "gradientThree activeTheme"
+              : "gradientThree"
+          }
+        ></span>
+      </div>
     </header>
   )
 }
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,35 +1,47 @@
-import React from 'react'
+import React from "react"
 
-const ShowTask = () => {
-  const tasks=[
-    {id:10001,name:"TASK A",time:"2:09:01 AM 9/10/2030"},
-    {id:10002,name:"TASK B",time:"2:09:05 AM 9/14/2030"},
-    {id:10003,name:"TASK C",time:"3:09:04 AM 9/18/2030"}
-    ]  
+const ShowTask = ({ tasklist, setTasklist, task, setTask }) => {
+  const handleEdit = (id) => {
+    const selectedTask = tasklist.find((todo) => todo.id === id)
+    setTask(selectedTask)
+  }
+  const handleDelete = (id) => {
+    const updatedTasklist = tasklist.filter((todo) => todo.id !== id)
+    setTasklist(updatedTasklist)
+  }
   return (
-    <section className='showTask'>
-        <div className='head'>
-            <div>
-                <span className='title'>Todo</span>
-                <span className='count'>0</span>
-            </div>
-            <button className='clearAll'>Clear All</button>
+    <section className="showTask">
+      <div className="head">
+        <div>
+          <span className="title">Todo</span>
+          <span className="count">{tasklist?.length}</span>
         </div>
-        <ul>
-            {
-                tasks.map(({id,name,time})=>{
-                    return(<li key={id}>
-                        <p>
-                            <span className='name'>{name}</span>
-                            <span className='time'>{time}</span>
-                        </p>
-                        <i className='bi bi-pencil-square'></i>
-                        <i className='bi bi-trash'></i>
-                    </li>)
-                })
-            }
-           
-        </ul>
+        <button
+          className="clearAll"
+          onClick={() => {
+            setTasklist([])
+          }}
+        >
+          Clear All
+        </button>
+      </div>
+      <ul>
+        {tasklist?.map(({ id, name, time }) => {
+          return (
+            <li key={id}>
+              <p>
+                <span className="name">{name}</span>
+                <span className="time">{time}</span>
+              </p>
+              <i
+                className="bi bi-pencil-square"
+                onClick={() => handleEdit(id)}
+              ></i>
+              <i className="bi bi-trash" onClick={() => handleDelete(id)}></i>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
